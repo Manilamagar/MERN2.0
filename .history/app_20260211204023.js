1ab550dd5fcfc808e5c9abaede5e9dec15ccebf3
Manilamagar <manilamagar200@gmail.com>
2026-02-11 20:40:42 +0545

    first commit

---
+++ .history/app_20260211204023.js
@@ -0,0 +1,38 @@
+const express = require('express');
+const app = express();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+app.listen(3000, () => {
+    console.log('Server is running on port 3000');
+});
+
+
+echo "# MERN2.0" >> README.md
+git init
+git add README.md
+git commit -m "first commit"
+git branch -M main
+git remote add origin https://github.com/Manilamagar/MERN2.0.git
+git push -u origin main
--- .history/app_20260211204028.js
+++ .history/app_20260211204028.js
@@ -30,3 +30,9 @@ app.listen(3000, () => {
 
 
 
+git init
+git add README.md
+git commit -m "first commit"
+git branch -M main
+git remote add origin https://github.com/Manilamagar/MERN2.0.git
+git push -u origin main
