3c9e0890b60677e34f8ec80940e73790e9bddb59
Manilamagar <manilamagar200@gmail.com>
2026-02-11 20:07:04 +0545

    first commit

---
+++ .history/app_20260211200656.js
@@ -0,0 +1,32 @@
+const express = require('express');
+const app = express();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+git init
+git add .
+git commit -m "first commit"
+git branch -M main
+git remote add origin https://github.com/Manilamagar/MERN-2.0-Workshop.git
+git push -u origin main
---
+++ .history/app_20260211200700.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const app = express();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
