cd4772d2432c503f6a734cbfbce4fa7728924e32
Manilamagar <manilamagar200@gmail.com>
2026-02-11 20:01:53 +0545

    Cleared old files

--- .history/database/index_20260210211836.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require("mongoose")
-const ConnectionString = "mongodb+srv://manilamagar80:manila123456789@cluster0.xwpoe.mongodb.net/?appName=Cluster0"
-async function connectToDatabase(){
-    await mongoose.connect(ConnectionString)
-    console.log("Connected To DB Successfully")
-}
-
-module.exports = connectToDatabase
